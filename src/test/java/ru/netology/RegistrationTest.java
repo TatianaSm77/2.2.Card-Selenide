f9a5a675eba83b4b1719d6d83346d744746d670c
Tatiana Smirnova <redkiivid@mail.ru>
2023-05-25 16:05:22 +0300

    New commit

--- src/test/java/ru/netology/RegistrationTest.java
+++ src/test/java/ru/netology/RegistrationTest.java
@@ -5,13 +5,13 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -25,19 +25,21 @@ public class RegistrationTest {
         Configuration.headless = true;
         open("http://localhost:9999/");
     }
-            @Test
-            void shouldBeValidCity() {
-                String meetingDate = getLocalDate(19);
-                $("[placeholder=Город]").setValue("Санкт-Петербург");
-                $("[data-test-id=\"date\"] span.input__box [placeholder=\"Дата встречи\"").doubleClick().sendKeys(meetingDate);
-                $x("//input[@name=\"name\"]").val("Иван Иванов");
-                $x("//*[@data-test-id=\"phone\"]/span/span/input").val("+79998887766");
-                $x("//*[@class=\"checkbox__text\"]").click();
-                $x("//*[@class=\"button__text\"]").click();
-                $x("//*[@data-test-id=\"notification\"]").should(visible, Duration.ofSeconds(15));
-                $x("//*[@class='notification__content']").
-                        shouldHave(Condition.text("Встреча успешно забронирована на " + meetingDate), Duration.ofSeconds(15));
-            }
+
+    @Test
+    void shouldBeValidCity() {
+        String meetingDate = getLocalDate(19);
+        $("[placeholder=Город]").setValue("Санкт-Петербург");
+        $("[data-test-id=\"date\"] span.input__box [placeholder=\"Дата встречи\"").doubleClick().sendKeys(meetingDate);
+        $x("//input[@name=\"name\"]").val("Иван Иванов");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("+79998887766");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $x("//*[@data-test-id=\"notification\"]").should(visible, Duration.ofSeconds(15));
+        $x("//*[@class='notification__content']").
+                shouldHave(Condition.text("Встреча успешно забронирована на " + meetingDate), Duration.ofSeconds(15));
+    }
+
     @Test
     void shouldTestNotValidCity() {
         String planningDate = getLocalDate(5);
@@ -50,6 +52,7 @@ public class RegistrationTest {
         $(withText("Доставка в выбранный город недоступна")).should(visible, Duration.ofSeconds(5));
         $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
     }
+
     @Test
     void shouldTestCityEnglish() {
         String planningDate = getLocalDate(6);
@@ -155,8 +158,8 @@ public class RegistrationTest {
         $(withText("только русские буквы, пробелы и дефисы")).should(visible);
         $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
     }
-    @Test
 
+    @Test
     void shouldTestPhoneWithoutPlus() {
         String planningDate = getLocalDate(90);
         $x("//input[@placeholder=\"Город\"]").val("Санкт-Петербург");
